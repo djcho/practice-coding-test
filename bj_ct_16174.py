cf8e25fad77e4e8a85eb0b88be29dd888e3629fc
조대준 <eowns36@gmail.com>
2022-06-29 23:48:53 +0900

    commit

--- bj_ct_16174.py
+++ bj_ct_16174.py
@@ -1,5 +1,33 @@
+from collections import deque
+
 N = int(input())
 MAP = [list(map(int, input().split())) for _ in range(N)]
+visited = [([False] * N) for _ in range(N)]
 
-for i in range(N):
-    print(MAP[i])
+def bfs(x,y):
+    queue = deque()
+    queue.append([x,y])
+
+    while(queue):
+        current = queue.popleft()
+        x = current[0]
+        y = current[1]
+
+        distance = MAP[x][y]
+        if distance == -1:
+            print("HaruHaru")
+            return
+        
+        # 인접한 다음 좌표들 구함
+        nextX = x + distance
+        nextY = y + distance
+        if (0 <= nextX < N) and visited[nextX][y] == False:
+            queue.append([nextX, y])
+            visited[nextX][y] = True           
+        if (0 <=  nextY < N) and visited[x][nextY] == False:
+            queue.append([x, nextY])
+            visited[x][nextY] = True
+    else:
+        print("Hing")
+
+bfs(0,0)
