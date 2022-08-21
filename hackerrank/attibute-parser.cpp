d6c0eb9db5b8d2abd7688e7dbbba39444ba0d0ca
조대준 <eowns36@gmail.com>
2022-08-22 01:07:06 +0900

    study

--- hackerrank/attibute-parser.cpp
+++ hackerrank/attibute-parser.cpp
@@ -1,44 +1,62 @@
+﻿// codingtest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <map>
 using namespace std;
+
+class TagNode
+{
+private:
+    std::vector<std::string> attributes;
+    TagNode* childNode;
+
+public:
+    TagNode(std::vector<std::string> attributes,TagNode* childNode)
+        :attributes(attributes), childNode(childNode)
+    {
+    }
+
+    TagNode* getChildNode() { return this->childNode; }
+    std::vector<std::string>& getAttributes() { return this->attributes; }
+    
+};
 
 class StringUtil
 {
 public:
-    static void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
+
+    std::string replace(const std::string& src, const std::string& from, const std::string& to) {
+
+        std::string replaced = src;
+        size_t start_pos = 0;
+        while ((start_pos = replaced.find(from, start_pos)) != std::string::npos) {
+            replaced.replace(start_pos, from.length(), to);
+            start_pos += to.length();
+        }
+        return replaced;
     }
 
-    static void rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(),
-                std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    }
-
-    static std::vector<std::string> splitString(const std::string &src, const std::string & delimiter, bool isTrim = false)
+    std::vector<std::string> splitString(const std::string& src, const std::string& delimiter)
     {
         std::vector<std::string> vec;
         std::string tmp_src = src;
         std::size_t pos;
-         while((pos = tmp_src.find(delimiter)) != std::string::npos)
+        while ((pos = tmp_src.find(delimiter)) != std::string::npos)
         {
             std::string splitStr = tmp_src.substr(0, pos);
-            if(isTrim)
-            {
-                StringUtil::ltrim(splitStr);
-                StringUtil::rtrim(splitStr);
-            }
-            std::cout << splitStr <<  std::endl;
+            std::cout << splitStr << std::endl;
             vec.push_back(splitStr);
-            
+
             tmp_src.erase(0, pos + delimiter.length());
         }
         vec.push_back(tmp_src);
-        std::cout << tmp_src <<  std::endl;
+        std::cout << tmp_src << std::endl;
+        return vec;
     }
 };
 
@@ -46,74 +64,128 @@ class HrmlParser
 {
 private:
     std::vector<std::string> sourceCode;
-    std::map<std::string, std::pair<std::string, std::string>> attributeMap;
-    
+    std::map<std::string, TagNode*> tagMap;
+
 public:
-    HrmlParser(std::vector<std::string> sourceCode) : sourceCode(sourceCode){}
-    ~HrmlParser(){}
-     
+    HrmlParser(std::vector<std::string> sourceCode) : sourceCode(sourceCode) {}
+    ~HrmlParser() {}
+
     std::string query(const std::string queryString)
     {
+        std::string src = queryString;
+        std::size_t pos = src.find_first_of(".~");
+        
+        StringUtil su;
+
+        TagNode* tagNode = nullptr;
+        while (pos != std::string::npos)
+        {
+            switch (src[pos])
+            {
+            case '.': 
+            {
+                std::string tagName = src.substr(0, pos);
+                if (!tagNode)
+                {
+                    std::map<std::string, TagNode*>::iterator iter = this->tagMap.find(tagName);
+                    if (iter != this->tagMap.end())
+                        tagNode = iter->second;
+                }
+                else
+                    tagNode = tagNode->getChildNode();
+                src.erase(0, pos + 1);
+                break;
+            }
+            case '~':                
+                std::string query = src.substr(0, pos);
+                std::vector<std::string> tagKeyValue = su.splitString(query, "~");
+                std::string tagName = tagKeyValue[0];
+                if (!tagNode)
+                {
+                    std::map<std::string, TagNode*>::iterator iter = this->tagMap.find(tagName);
+                    if (iter != this->tagMap.end())
+                        tagNode = iter->second;
+                }
+
+                std::string attributeName = tagKeyValue[1];
+                for (auto& attributeKeyValue : tagNode->getAttributes())
+                {
+                    std::vector<std::string> splitedVec = su.splitString(attributeKeyValue, " = ");
+                    if(splitedVec[0] == attributeName)
+                    {
+                        return splitedVec[1];
+                    }
+                }
+                return std::string("Not Found!");
+            }
+        }
+        
+
+
         return std::string();
     }
-    
+
 
     std::string getTagName(const std::string& tag)
     {
         //<tag1 value = "HelloWorld">
         std::size_t found = tag.find('<');
-        std::string tagName = tag.substr(found + 1, tag.find(' ', found));
+        std::string tagName = tag.substr(found + 1, tag.find(' ', found) - 1);
         return tagName;
     }
-    
-    std::pair<std::string, std::string> parse()
-    {
-        std::string tagName;
-        std::vector<std::pair<std::string, std::string>> attributes;
-        for(auto & code : this->sourceCode)
-        {
-            if(this->isCloser(code))
-                continue;
-            
-            tagName = getTagName(code);
-            if(tagName.empty())
-                continue;
-            
-            attributes = getAttributeNames(code);
-            for(auto& attribute : attributes)
-            {
-                std::pair<std::string, std::pair<std::string, std::string>> tag(tagName, attribute);
-                this->attributeMap.insert(tag);
-            }            
-        }
-    }
 
-    std::vector<std::pair<std::string, std::string>> getAttributeNames(const std::string& tag)
+     std::vector<std::string> getAttributeNames(const std::string& tag)
     {
         //<tag3 another="another" final = "final">
-        std::vector<std::pair<std::string, std::string>> attributes;
-
         std::size_t found = tag.find(' ');
-        std::string attributeStrings = tag.substr(found + 1, tag.find_last_of('>'));
-
-        std::vector<std::string> splitAttributes = StringUtil::splitString(attributeStrings, """ ");
-        for(auto& splitAttribut : splitAttributes)
-        {
-            std::vector<std::string> keyValue = StringUtil::splitString(splitAttribut, "=", true);    
-            attributes.push_back(std::pair<std::string, std::string>(keyValue[0], keyValue[1]));
-        }
+        std::string attributeStrings = tag.substr(found + 1, tag.find_last_of('>') - (found + 1));
         
-        return attributes; 
+        StringUtil su;
+
+        std::vector<std::string> splitedAttributes = su.splitString(attributeStrings, "\" ");
+        
+        for(auto& splitedAttribute : splitedAttributes)
+            splitedAttribute.erase(std::remove(splitedAttribute.begin(), splitedAttribute.end(), '\"'), splitedAttribute.end());
+
+        return splitedAttributes;
     }
 
+    void parse(TagNode* parentTagNode, std::vector<std::string>::iterator iter)
+    {
+        if (iter == this->sourceCode.end())
+            return;
+
+        std::string code = *iter;
+
+        if (!this->isCloser(code))
+        {
+            std::string tagName = getTagName(code);
+            std::vector<std::string> attributes = getAttributeNames(code);
+
+            TagNode* tagNode = new TagNode(attributes, parentTagNode);
+            std::pair<std::string, TagNode*> tag(tagName, tagNode);
+            this->tagMap.insert(tag);
+        }
+
+        iter++;
+        parse(nullptr, iter);
+    }
+
+    void parse()
+    {
+        TagNode* parentTagNode = nullptr;
+        std::vector<std::string>::iterator iter = this->sourceCode.begin();
+
+        parse(nullptr, iter);
+    }
 
     bool isCloser(const std::string& tag)
     {
-        if(tag.empty())
+        if (tag.empty())
             return false;
-        
+
         std::size_t found = tag.find("</");
-        if(found != std::string::npos)
+        if (found != std::string::npos)
             return true;
         else
             return false;
@@ -124,45 +196,33 @@ public:
 std::vector<std::string> inputCode(int inputLine)
 {
     std::string lineOfCode;
-     std::vector<std::string> intputData;
-     
-    for(int i = 0; i < inputLine; ++i)
+    std::vector<std::string> intputData;
+
+    for (int i = 0; i < inputLine; ++i)
     {
-        std::cin >> lineOfCode;
-        
+        std::getline(std::cin, lineOfCode);
         intputData.push_back(lineOfCode);
     }
-    
+
     return intputData;
 }
 
-void printVector(const std::vector<std::string>& vec)
-{
-    for(auto& str : vec)
-    {
-        std::cout << str << std::endl;
-    }
-}
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int sourceLine = 0;
     int queryLine = 0;
 
     std::cin >> sourceLine >> queryLine;
     std::cin.ignore();
-    
+
     std::vector<std::string> sourceCode = inputCode(sourceLine);
     std::vector<std::string> queryCode = inputCode(queryLine);
-    
-    printVector(sourceCode);
-    printVector(queryCode);
-    
+
     HrmlParser hrmlParser(sourceCode);
     hrmlParser.parse();
-    for(auto& query : queryCode)
+    for (auto& query : queryCode)
         std::cout << hrmlParser.query(query) << std::endl;
-    
+
     return 0;
 }
 
