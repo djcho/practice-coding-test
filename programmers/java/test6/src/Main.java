97f284ad5bd2b75c6f85b8358ac50370ee50e725
djcho <eowns36@gmail.com>
2022-11-25 23:29:45 +0900

    commit

---
+++ programmers/java/test6/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(solution("1924", 2));
+    }
+
+    public static String solution(String number, int k) {
+        String answer = "";
+
+        for(int i = 0; i < number.length(); i++){
+            for(int j = i + 1; j < number.length(); j++){
+                Character.getNumericValue(number.charAt(i));    
+            }
+
+        }
+
+        return answer;
+    }
+
+}
